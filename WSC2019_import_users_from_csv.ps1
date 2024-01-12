591eeb58f15e299e1fdf303c06549caa523a1437
kleberDevRedes <kleberolivera12@gmail.com>
2024-01-12 14:46:54 -0300

    :sparkles: feat:Incluindo nova automação para criação de usuarios atravez de um arquivo csv

---
+++ WSC2019_import_users_from_csv.ps1
@@ -0,0 +1,32 @@
+import-module Active-Diretory
+
+$File = Import-Csv -delimiter ';' -path c:\WSC2019_TP39_Module_B_Users.csv
+
+$File|Select-Object|Sort-Object 'Organizational Unit' -Unique|Select-Object 'Organizational Unit'|ForEach-Object {
+    New-ADOrganizationalUnit -Name $_.'Organizational Unit'
+}
+
+$File|Select-Object|Sort-Object 'Group','Organizational Unit' -Unique|Select-Object 'Group','Organizational Unit'|ForEach-Object {
+    New-ADGroup -Name $_.Group -Path "OU=$($_.'Organizational Unit'),dc=wsc2019,dc=ru"
+}
+
+$File|ForEach-Object {
+    New-ADUser `
+    -Name ($_.'Given Name'+' '+$_.Surname) `
+    -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) `
+    -City $_.City `
+    -Company $_.Company `
+    -OtherAttributes @{co=$_.Country}`
+    -DisplayName ($_.'Given Name'+' '+$_.Surname) `
+    -EmailAddress $_.'E-Mail' `
+    -Enabled $true `
+    -GivenName $_.'Given Name' `
+    -HomeDirectory '\\dc2\Homes\%username%' `
+    -HomeDrive 'G:\' `
+    -PasswordNeverExpires $true `
+    -Path ("ou="+$_."organizational unit"+",dc=wsc2019,dc=ru") `
+    -SamAccountName $_.ID `
+    -Surname $_.Surname
+    Add-ADGroupMember -Identity $_.Group -Members $_.IDz
+}
+
